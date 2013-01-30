eb99c0ed339299e5395a3ffe06445eeab545057e
Nathan Hopkins <natehop@gmail.com>
2013-01-30 11:05:05 -0700

    Added some docs to Ticket.

--- lib/ellington/ticket.rb
+++ lib/ellington/ticket.rb
@@ -1,6 +1,17 @@
 require "delegate"
 
 module Ellington
+
+  # Simple wrapper around a ticket object.
+  # NOTE: This may not be needed but it does preserve some options for the future.
+  # @example
+  #   TicketData = Struct.new(:user_id, :debit_id, :credit_id)
+  #   ticket_data = TicketData.new(
+  #     "458e9710-c1c4-495b-b25a-c622e893f6f7",
+  #     "b834fb69-2eb3-4e57-b313-16b863e91f74",
+  #     "9a670fce-4826-4a90-aaf7-a58738a4ce5b"
+  #   )
+  #   ticket = Ellington::Ticket.new(ticket_data)
   class Ticket < SimpleDelegator
 
     def initialize(context)
@@ -8,4 +19,5 @@ module Ellington
     end
 
   end
+
 end
