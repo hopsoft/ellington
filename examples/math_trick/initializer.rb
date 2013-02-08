61769acd8ddeba2577cc85254cfb07d43bc314b6
Nathan Hopkins <natehop@gmail.com>
2013-02-08 08:54:23 -0700

    More work on setting up the proper dir structure.

--- examples/math_trick/initializer.rb
+++ examples/math_trick/initializer.rb
@@ -1,9 +1,3 @@
-require "logger"
-require_relative "../../lib/ellington"
-require_relative "conductor"
-stations_path = File.expand_path("stations", File.dirname(__FILE__))
-Dir[File.join(stations_path, "*.rb")].each { |station| require station }
-
 # Use ellington to model this process
 #
 # 0 - Pick any three digits number with decreasing digits (432 or 875) [This will be implicit, thus step #0]
@@ -15,31 +9,20 @@ Dir[File.join(stations_path, "*.rb")].each { |station| require station }
 # You will get a result of 1089
 # credit: http://www.basic-mathematics.com/number-trick-with-1089.html
 
+require "logger"
+require_relative "../../lib/ellington"
+require_relative "states"
+require_relative "conductor"
+stations_path = File.expand_path("stations", File.dirname(__FILE__))
+Dir[File.join(stations_path, "*.rb")].each { |station| require station }
 Ellington.logger = Logger.new($stdout)
 
-# configure the states & their transitions
-states = StateJacket::Catalog.new
-states.add :new_number => [:first_reverse_pass, :first_reverse_fail, :first_reverse_error]
-states.add :first_reverse_fail
-states.add :first_reverse_error
-states.add :first_reverse_pass => [:subtract_pass, :subtract_fail, :subtract_error]
-states.add :subtract_fail
-states.add :subtract_error
-states.add :subtract_pass => [:second_reverse_pass, :second_reverse_fail, :second_reverse_error]
-states.add :second_reverse_fail
-states.add :second_reverse_error
-states.add :second_reverse_pass => [:add_pass, :add_fail, :add_error]
-states.add :add_fail
-states.add :add_error
-states.add :add_pass
-
 # setup the goals
-ticket_goal = Ellington::Goal.new(:add_pass)
 route_goal = Ellington::Goal.new(:add_pass)
 line_goal = Ellington::Goal.new(:add_pass)
 
 # configure the route
-route = Ellington::Route.new('Math Trick', states, route_goal)
+route = Ellington::Route.new('Math Trick', MathTrick::States, route_goal)
 line = Ellington::Line.new('Get to 1089', line_goal)
 line << MathTrick::FirstReverseStation.new
 line << MathTrick::SubtractStation.new
@@ -47,15 +30,10 @@ line << MathTrick::SecondReverseStation.new
 line << MathTrick::AddStation.new
 route.add line
 
-# -----------------------------------------------------------------------------------------------------
-
-# manually put passengers on the route
-conductor = MathTrick::Conductor.new(route)
-[631, 531, 955, 123].each do |number|
-  puts "\n\n"
-  ticket = Ellington::Ticket.new(ticket_goal)
-  passenger = Ellington::Passenger.new(number, ticket, route.states)
-  passenger.current_state = :new_number
-  conductor.escort passenger
+module MathTrick
+  class << self
+    attr_accessor :route
+  end
 end
+MathTrick.route = route
 
