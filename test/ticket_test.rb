9950b69253a385ec7cd04f7956b9caef4bcaa258
Nathan Hopkins <natehop@gmail.com>
2013-02-14 14:56:41 -0700

    Got the ticket tests working.

--- test/ticket_test.rb
+++ test/ticket_test.rb
@@ -4,12 +4,13 @@ require "digest"
 class TicketTest < MicroTest::Test
 
   before do
-    @number = NumberWithHistory.new(0)
-    @ticket = Ellington::Ticket.new(Ellington::Target.new(:closed), 
+    @route = BasicMath.new
+    @ticket = Ellington::Ticket.new(Ellington::Target.new(:closed),
       :user_id => "458e9710-c1c4-495b-b25a-c622e893f6f7",
       :debit_id => "b834fb69-2eb3-4e57-b313-16b863e91f74",
       :credit_id => "9a670fce-4826-4a90-aaf7-a58738a4ce5b"
     )
+    @passenger = Ellington::Passenger.new(0, @route, @ticket)
   end
 
   test "basic ticket" do
@@ -22,40 +23,17 @@ class TicketTest < MicroTest::Test
     assert @ticket.id == Digest::SHA256.hexdigest(@ticket.values.map(&:to_s).join)
   end
 
-  test "passenger assignment" do
-    passenger = Ellington::Passenger.new(@number, Ellington::Ticket.new, StateJacket::Catalog.new)
-    @ticket.passenger = passenger
-    assert @ticket.passenger == passenger
-  end
-
-  test "passenger assignment is only allowed once" do
-    passenger = Ellington::Passenger.new(@number, Ellington::Ticket.new, StateJacket::Catalog.new)
-    error = nil
-    begin
-      @ticket.passenger = passenger
-      @ticket.passenger = passenger
-    rescue Ellington::PassengerAlreadyAssignedToTicket => e
-      error = e
-    end
-    assert !error.nil?
-  end
-
-  test "goal is not achieved when no passenger assigned" do
-    assert !@ticket.goal_achieved?
+  test "goal" do
+    assert @ticket.goal.to_a == [:closed]
   end
 
   test "goal is not achieved when passenger is not in the correct state" do
-    passenger = Ellington::Passenger.new(@number, Ellington::Ticket.new, StateJacket::Catalog.new)
-    passenger.current_state = :open
-    @ticket.passenger = passenger
-    assert !@ticket.goal_achieved?
+    assert !@ticket.goal.satisfied?(@passenger)
   end
 
   test "goal is achieved when passenger is in the correct state" do
-    passenger = Ellington::Passenger.new(@number, Ellington::Ticket.new, StateJacket::Catalog.new)
-    passenger.current_state = :closed
-    @ticket.passenger = passenger
-    assert @ticket.goal_achieved?
+    @passenger.current_state = :closed
+    assert @ticket.goal.satisfied?(@passenger)
   end
 
 end
