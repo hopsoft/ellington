0d877de2a82e38cefe6a23210ad1a90fed7cc1f1
Nathan Hopkins <natehop@gmail.com>
2013-02-15 11:55:20 -0700

    Added test for connection.

---
+++ test/connection_test.rb
@@ -0,0 +1,19 @@
+require_relative "test_helper"
+
+class ConnectionTest < MicroTest::Test
+
+  before do
+    @route = BasicMath.new
+    @line = @route.lines.first
+    @connection = Ellington::Connection.new(@route.lines[1], @line.passed)
+  end
+
+  test "line" do
+    assert @connection.line == @route.lines[1]
+  end
+
+  test "states" do
+    assert @connection.states == @line.passed
+  end
+
+end
